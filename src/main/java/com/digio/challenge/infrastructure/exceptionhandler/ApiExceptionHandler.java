8d051abcb6e156488119b6a33a23d3a826d7b8a4
Marco Santarelle da Silva Jardim <944513@lojasrenner.com.br>
2024-08-03 22:00:49 -0300

    feat: add api exception handler

---
+++ src/main/java/com/digio/challenge/infrastructure/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.digio.challenge.infrastructure.exceptionhandler;
+
+import com.digio.challenge.application.exception.BusinessLogicException;
+import com.digio.challenge.application.exception.ProductNotFoundException;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Log4j2
+@ControllerAdvice
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(BusinessLogicException.class)
+    public ResponseEntity<Object> handleBusinessLogicException(BusinessLogicException ex, WebRequest webRequest) {
+        HttpStatusCode httpStatusCode = HttpStatusCode.valueOf(HttpStatus.INTERNAL_SERVER_ERROR.value());
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .title(httpStatusCode.toString())
+                .status(httpStatusCode.value())
+                .detail(ex.getMessage())
+                .build();
+        log.error("Handler error business logic exception {}", errorResponse, ex);
+        return handleExceptionInternal(ex, errorResponse, new HttpHeaders(), httpStatusCode, webRequest);
+    }
+
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<Object> handleProductNotFoundException(ProductNotFoundException ex, WebRequest webRequest) {
+        HttpStatusCode httpStatusCode = HttpStatusCode.valueOf(HttpStatus.NOT_FOUND.value());
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .title(httpStatusCode.toString())
+                .status(httpStatusCode.value())
+                .detail(ex.getMessage())
+                .build();
+        log.error("Handler error product not found exception {}", errorResponse);
+        return handleExceptionInternal(ex, errorResponse, new HttpHeaders(), httpStatusCode, webRequest);
+    }
+
+}

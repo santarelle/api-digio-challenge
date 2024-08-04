dbc0ee1d2821574b1ee95bcfb0693dc7128aa03f
Marco Santarelle da Silva Jardim <944513@lojasrenner.com.br>
2024-08-03 21:39:48 -0300

    feat: add ObjectMapperUtils for testing

---
+++ src/test/java/com/digio/challenge/utils/ObjectMapperUtils.java
@@ -0,0 +1,26 @@
+package com.digio.challenge.utils;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+
+@Log4j2
+public final class ObjectMapperUtils {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static <T> T map(TypeReference<T> reference, ResultActions resultActions) {
+        try {
+            String contentAsString = resultActions.andReturn().getResponse().getContentAsString(StandardCharsets.UTF_8);
+            return objectMapper.readValue(contentAsString, reference);
+        } catch (UnsupportedEncodingException | JsonProcessingException ex) {
+            log.error("Error to convert result actions to a reference {}", reference, ex);
+            throw new RuntimeException(ex);
+        }
+    }
+}

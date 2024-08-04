fcd6f044d1b0352e0f39af452332ec355be0af14
Marco Santarelle da Silva Jardim <944513@lojasrenner.com.br>
2024-08-03 21:37:45 -0300

    chore: enable feign client in main class

--- src/main/java/com/digio/challenge/ApiDigioChallengeApplication.java
+++ src/main/java/com/digio/challenge/ApiDigioChallengeApplication.java
@@ -2,12 +2,14 @@ package com.digio.challenge;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ApiDigioChallengeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiDigioChallengeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDigioChallengeApplication.class, args);
+    }
 
 }

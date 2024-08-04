0537b7c49f79c6b096625514fe907c5d56a74efa
Marco Santarelle da Silva Jardim <944513@lojasrenner.com.br>
2024-08-03 22:39:23 -0300

    test: add purchase controller impl test to integration test suite

--- src/test/java/com/digio/challenge/IntegrationTests.java
+++ src/test/java/com/digio/challenge/IntegrationTests.java
@@ -1,11 +1,13 @@
 package com.digio.challenge;
 
+import com.digio.challenge.adapter.in.web.impl.PurchaseControllerImplTest;
 import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 import org.junit.platform.suite.api.SuiteDisplayName;
 
 @Suite
 @SelectClasses({
+        PurchaseControllerImplTest.class
 })
 @SuiteDisplayName("Suite that brings all integration tests")
 @SuppressWarnings("squid:S2187")

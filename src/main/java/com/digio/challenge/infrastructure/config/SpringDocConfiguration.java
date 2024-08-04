1818efed1eaa116b173103a2a731b7ecaf862057
Marco Santarelle da Silva Jardim <944513@lojasrenner.com.br>
2024-08-03 21:42:36 -0300

    docs: configure open api

---
+++ src/main/java/com/digio/challenge/infrastructure/config/SpringDocConfiguration.java
@@ -0,0 +1,27 @@
+package com.digio.challenge.infrastructure.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfiguration {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Api DIGIO Challenge")
+                        .version("1.0.0")
+                        .description("Microservice REST API with specific endpoints that return the " +
+                                "analyze customers' wine purchases, providing detailed information about purchases, " +
+                                "performs annual analyses, and identifies the most loyal customers.")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://springdoc.org")
+                        )
+                );
+    }
+}

45c93672867f60be90eb19facdcd39b3393800c6
Marco Santarelle da Silva Jardim <944513@lojasrenner.com.br>
2024-08-03 21:10:28 -0300

    chore: initial commit

---
+++ src/main/java/com/digio/challenge/ApiDigioChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.digio.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDigioChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDigioChallengeApplication.class, args);
+	}
+
+}
